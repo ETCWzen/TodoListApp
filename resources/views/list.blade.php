0cc823fe84c88e8860e36c3e463108ca1dd8d754
Eric <microgreens.zen@gmail.com>
2023-08-29 12:53:53 -0400

    first commit with home page and list page

---
+++ resources/views/list.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Your List</title>
+</head>
+<body>
+<h1>To Do List</h1>
+
+<div class="highPriority"></br>
+    <label for="highTask">High Priority Tasks</label></br>
+    <input type="text" name="highTask">
+</div>
+
+<div class="mediumPriority"></br>
+    <label for="mediumTask">Medium Priority Tasks</label></br>
+    <input type="text" name="mediumTask">
+</div>
+
+<div class="lowPriority"></br>
+    <lable for="lowTask">No Priority Tasks</lable></br>
+    <input type="text" name="lowTask">
+</div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>To Do List</title>
+    </head>
+    <body>
+    <h1>To Do List</h1>
+    <a href="/list">Go to your Tasks List</a>
+    </body>
+</html>
