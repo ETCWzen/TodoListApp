1a6dd23e8803cc4a39c0cee67657f4f0573b1d52
Eric <microgreens.zen@gmail.com>
2023-08-30 10:20:20 -0400

    added new page and submit buttons

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -12,16 +12,19 @@
 <div class="highPriority"></br>
     <label for="highTask">High Priority Tasks</label></br>
     <input type="text" name="highTask">
+    <input type="submit" value="Submit">
 </div>
 
 <div class="mediumPriority"></br>
     <label for="mediumTask">Medium Priority Tasks</label></br>
     <input type="text" name="mediumTask">
+    <input type="submit" value="Submit">
 </div>
 
 <div class="lowPriority"></br>
     <lable for="lowTask">No Priority Tasks</lable></br>
     <input type="text" name="lowTask">
+    <input type="submit" value="Submit">
 </div>
 </body>
 </html>
